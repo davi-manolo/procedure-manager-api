070de57a74de96c9135f28a78fcd1bf0c76efc9c
Davi Manolo <davi.manolo@gmail.com>
2023-01-17 23:32:09 -0300

    Started the implementation of share file system.

--- src/main/java/com/procedure/manager/service/creator/XlsCreator.java
+++ src/main/java/com/procedure/manager/service/creator/XlsCreator.java
@@ -3,6 +3,8 @@ package com.procedure.manager.service.creator;
 import com.procedure.manager.domain.exception.WorkbookException;
 import com.procedure.manager.domain.vo.DataContentProcedureVo;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
@@ -19,11 +21,21 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class XlsCreator {
 
     private static final String PROCEDURE_NAME = "Procedimentos";
+    private static final String TITLE = "Relatórios de Procedimentos";
+    private static final String PROCEDURE_DATE = "Data do Procedimento";
+    private static final String CUSTOMER = "Cliente";
+    private static final String PROCEDURE_TYPE = "Tipo de Procedimento";
+    private static final String PROCEDURE_VALUE = "R$ Valor Procedimento";
+    private static final String RECEIVED_VALUE = "R$ Valor Recebido";
+    private int lastRow = 0;
 
     public byte[] create(List<DataContentProcedureVo> dataContentProcedureVoList) {
         try(Workbook workbook = new HSSFWorkbook()) {
             Sheet sheet = workbook.createSheet(PROCEDURE_NAME);
+            lastRow = sheet.getLastRowNum();
             applyWorksheetSettings(sheet);
+            createHeader(sheet);
+            createContent(sheet, dataContentProcedureVoList);
             return workbookToByteArray(workbook);
         } catch (IOException e) {
             throw new WorkbookException(INTERNAL_SERVER_ERROR, WORKBOOK_IO_ERROR);
@@ -43,4 +55,56 @@ public class XlsCreator {
         sheet.setDefaultColumnWidth(20);
         sheet.setDefaultRowHeight((short) 600);
     }
+
+    private void createHeader(Sheet sheet) {
+        Row rowTitle = createRow(sheet);
+        Cell cellTitle = rowTitle.createCell(0);
+        cellTitle.setCellValue(TITLE);
+
+        Row rowSubtitles = createRow(sheet);
+
+        Cell cellProcedureDate = rowSubtitles.createCell(0);
+        cellProcedureDate.setCellValue(PROCEDURE_DATE);
+
+        Cell cellCustomer = rowSubtitles.createCell(1);
+        cellCustomer.setCellValue(CUSTOMER);
+
+        Cell cellProcedureType = rowSubtitles.createCell(2);
+        cellProcedureType.setCellValue(PROCEDURE_TYPE);
+
+        Cell cellValue = rowSubtitles.createCell(3);
+        cellValue.setCellValue(PROCEDURE_VALUE);
+
+        Cell cellValueReceived = rowSubtitles.createCell(4);
+        cellValueReceived.setCellValue(RECEIVED_VALUE);
+    }
+
+    private void createContent(Sheet sheet, List<DataContentProcedureVo> dataContentProcedureVoList) {
+
+        dataContentProcedureVoList.forEach(contentLine -> {
+
+            Row rowContent = createRow(sheet);
+
+            Cell cellProcedureDate = rowContent.createCell(0);
+            cellProcedureDate.setCellValue(contentLine.getProcedureDate());
+
+            Cell cellCustomer = rowContent.createCell(1);
+            cellCustomer.setCellValue(contentLine.getCustomer());
+
+            Cell cellProcedureType = rowContent.createCell(2);
+            cellProcedureType.setCellValue(contentLine.getProcedureTypeName());
+
+            Cell cellValue = rowContent.createCell(3);
+            cellValue.setCellValue(contentLine.getProcedureValue());
+
+            Cell cellValueReceived = rowContent.createCell(4);
+            cellValueReceived.setCellValue(contentLine.getValueReceived());
+
+        });
+    }
+
+    private Row createRow(Sheet sheet) {
+        return sheet.createRow(++lastRow);
+    }
+
 }
